3637bac7474a0d80755df88f2f4f6833a172288f
iwezeneneh55-ux <iwezeneneh55@gmail.com>
2026-03-25 13:13:04 +0000

    first commit

---
+++ src/script.js
@@ -0,0 +1,37 @@
+let name = "Princess";
+console.log(name);
+
+let age = 40;
+console.log(age);
+
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log(sum);
+
+let difference = num2 - num1;
+console.log(difference);
+
+let quotient = num2 / num1;
+console.log(quotient);
+
+let product = num1 * num2;
+console.log(product);
+
+let num3 = 15;
+let num4 = 4;
+let remainder = num3 % num4;
+console.log(remainder);
+
+let x = true;
+let y = false;
+console.log(x);
+console.log(y);
+
+let a = 5;
+typeof(a);
+console.log(typeof(a));
+
+let b = "Hello";
+typeof(b);
+console.log(typeof(b));
