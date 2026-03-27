25079c37a3790fb3ba9060868d651d7ce6e3807b
iwezeneneh55-ux <iwezeneneh55@gmail.com>
2026-03-27 13:32:48 +0000

    adding all changes

--- src/script.js
+++ src/script.js
@@ -35,3 +35,31 @@ console.log(typeof(a));
 let b = "Hello";
 typeof(b);
 console.log(typeof(b));
+let number = 7
+//prompt the user to enter a number
+//check if the numberis even or odd
+//Display that the number the user entered is even or odd
+// prompt("Enter a number")
+// let num6 = prompt("Enter a number")
+// if (num6 % 2 == 0){
+//     alert(`number ${num6} is even`)
+// }else{
+//     alert(`number ${num6} is odd`)
+// }
+//take the user's name and also ask for the age
+//if they are 18years and above, tell them they can sign up for tiktok
+//if they are less than 18 years, tell them that they cannot sign up for tiktok at this time
+//
+let appointment = prompt("Would you like to cancel, confirm or reschedule your appointment?");
+function bookAppointment() {
+    if (appointment === "cancel") {
+        alert("Your appointment has been cancelled.");
+    }
+    else if (appointment === "confirm") {
+            alert("see you soon");
+         } 
+    else if (appointment === "reschedule") {
+            alert("Please call our customer service to reschedule your appointment.");
+         }
+}
+
